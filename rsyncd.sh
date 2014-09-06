bd708816dd84beaff955e0653f0994be280e4d6b
Sun Smallwhite <niasw@pku.edu.cn>
2014-09-06 10:17:15 +0800

    add option function

--- rsyncd.sh
+++ rsyncd.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 #
 #  description: a script to rsync with my htc phone
-rsync -e 'ssh -p 8090' --rsync-path=/data/data/com.spartacusrex.spartacuside/files/system/bin/rsync -av niasw@192.168.32.32:/data/data/com.spartacusrex.spartacuside/files/syncfolder ~/
-rsync -e 'ssh -p 8090' --rsync-path=/data/data/com.spartacusrex.spartacuside/files/system/bin/rsync -av ~/syncfolder niasw@192.168.32.32:/data/data/com.spartacusrex.spartacuside/files/
+#  developer: sun.niasw@gmail.com
+
+function usage
+{
+    echo -e "Usage:\n\t$0\t[-s] [-r] [-h]"
+    echo -e "\t -s\tsend to  external device"
+    echo -e "\t -r\tget from external device"
+    echo -e "\t -h\thelp, print this message then exit"
+}
+
+function sendfiles
+{
+  rsync -e 'ssh -p 8090' --rsync-path=/data/data/com.spartacusrex.spartacuside/files/system/bin/rsync -av ~/syncfolder niasw@192.168.32.32:/data/data/com.spartacusrex.spartacuside/files/
+}
+
+function getfiles
+{
+  rsync -e 'ssh -p 8090' --rsync-path=/data/data/com.spartacusrex.spartacuside/files/system/bin/rsync -av niasw@192.168.32.32:/data/data/com.spartacusrex.spartacuside/files/syncfolder ~/
+}
+
+while getopts "srh" option
+do
+    case $option in
+        s   )   sendfiles;;
+        r   )   getfiles;;
+        *   )   usage
+                exit 1;;
+    esac
+done
