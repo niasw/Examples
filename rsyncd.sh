0885ab82df688d0060abe21ffdcc08720f675887
Sun Smallwhite <niasw@pku.edu.cn>
2014-09-08 08:35:36 +0800

    small patch

--- rsyncd.sh
+++ rsyncd.sh
@@ -21,12 +21,21 @@ function getfiles
   rsync -e 'ssh -p 8090' --rsync-path=/data/data/com.spartacusrex.spartacuside/files/system/bin/rsync -av niasw@192.168.32.32:/data/data/com.spartacusrex.spartacuside/files/syncfolder ~/
 }
 
+op=''
+
 while getopts "srh" option
 do
     case $option in
-        s   )   sendfiles;;
-        r   )   getfiles;;
+        s   )   op='s'
+                sendfiles;;
+        r   )   op='r'
+                getfiles;;
         *   )   usage
                 exit 1;;
     esac
 done
+
+if ! [[ -n $op ]]; then
+  echo "Sorry, I didn't get enough information."
+  usage
+fi
